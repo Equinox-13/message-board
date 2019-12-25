acd82c59281eacae6e2c0c11b77c013141d2b8c0
Equinox-13 <quaid.johar33@gmail.com>
2019-12-25 17:45:34 +0530

    Initial commit

---
+++ posts/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Post
+
+class HomePageView(ListView):
+	model = Post
+	template_name = 'home.html'
+	context_object_name = 'all_posts_list'
